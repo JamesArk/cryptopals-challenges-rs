63aaf9ad8698cdda0e06511caca029977ceb08f6
Tiago Guerreiro <tiago.guerreiro@marionete.co.uk>
2025-07-14 11:25:37 +0100

    first challenge done

--- set-01/01-hex-to-base64/hex-to-64/src/main.rs
+++ set-01/01-hex-to-base64/hex-to-64/src/main.rs
@@ -38,34 +38,30 @@ fn main() {
     let input = stdin();
     loop {
         let mut buf = String::new();
+        println!("Please provide a hex string:");
         input
             .read_line(&mut buf)
             .expect("Error occured while reading line");
         buf.pop(); // removes new line
-
         let hex_bytes = hex_to_bytes(&buf).unwrap();
-        println!("Binary representation of {:?} is:", buf);
+        println!("Binary representation:");
         let mut out = String::new();
         for b in hex_bytes.clone() {
-            out.push_str(&format!("{:04b} {:04b} ", b >> 4, b & 0x0f));
-        }
-        out.pop();
-        out.push_str("\n");
-        for b in hex_bytes {
             out.push_str(&format!("{:08b}", b));
         }
-        println!("{}", out)
+        println!("{}", out);
+        println!("Hex string: {}",buf);
+        println!("Base64: {}",bytes_to_base64(hex_bytes));
     }
 }
 
 const BASE64_SYMBOLS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 const HEX_SYMBOLS: &str = "0123456789ABCDEF";
 
+
 // fn hex_to_base64(hex_string: &String) -> Result<String,Error> {
 
 // }
-//  shift 4
-// 0000 0001 -> 0001 0000
 
 fn hex_to_bytes(hex_string: &String) -> Result<Vec<u8>, BadHexError> {
     if hex_string.len() % 2 == 1 {
@@ -104,6 +100,44 @@ fn hex_to_bytes(hex_string: &String) -> Result<Vec<u8>, BadHexError> {
     Ok(res)
 }
 
-// fn bytes_to_base64(bytes: &[u8]) -> String {
+fn bytes_to_base64(bytes: Vec<u8>) -> String {
+  let mut symbols:Vec<char> = Vec::new();
+  let mut current_byte = 0;
+  if bytes.len() == 0 {
+    return "not valid".to_owned();
+  } else if bytes.len() > 4 {
+    // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 ** 1111| 1111 ** 11|11 1111  % 4 = 2
+    // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 ** 1111| 1111 % 4 = 1
+    // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 % 4 = 0
+    // 1111 11|11 ** 1111| 1111 ** 11|11 1111  % 4 = 3
+    // 1111 11|11 ** 1111| 1111 % 4 = 2
+    // 1111 11|11 % 4 = 1
+    for i in (0..bytes.len()-2).step_by(3) {
+      let idx1:usize = ((bytes[i] & 0b1111_1100) >> 2) as usize;
+      let idx2:usize = ((bytes[i] & 0b0000_0011) << 4) as usize + ((bytes[i+1] & 0b1111_0000) >> 4) as usize;
+      let idx3:usize = ((bytes[i+1] & 0b0000_1111) << 2) as usize + ((bytes[i+2] & 0b1100_0000) >> 6) as usize;
+      let idx4:usize = ((bytes[i+2] & 0b0011_1111)) as usize;
 
-// }
+      symbols.push(BASE64_SYMBOLS.chars().nth(idx1).unwrap());
+      symbols.push(BASE64_SYMBOLS.chars().nth(idx2).unwrap());
+      symbols.push(BASE64_SYMBOLS.chars().nth(idx3).unwrap());
+      symbols.push(BASE64_SYMBOLS.chars().nth(idx4).unwrap());
+      current_byte += 3;
+    }
+
+  }
+  if bytes.len() % 3 == 1 {
+    // padding twice
+    symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b1111_1100) >> 2) as usize).unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_0011) << 4) as usize).unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
+  } else if bytes.len() % 3 == 2 {
+    // padding once
+    symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b1111_1100) >> 2) as usize).unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_0011) << 4) as usize + ((bytes[current_byte+1] & 0b1111_0000) >> 4) as usize).unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_1111) << 2) as usize).unwrap());
+    symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
+  }
+  return symbols.iter().collect();
+}
