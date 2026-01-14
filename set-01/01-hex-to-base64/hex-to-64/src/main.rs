5f8148af566a4f944479fcfd3255cb993dd29f1f
Tiago Guerreiro <tiago.guerreiro@marionete.co.uk>
2026-01-14 14:56:29 +0000

    validated hex to bin and hex to base64

--- set-01/01-hex-to-base64/hex-to-64/src/main.rs
+++ set-01/01-hex-to-base64/hex-to-64/src/main.rs
@@ -1,14 +1,78 @@
-use std::time;
+use std::{fs::File, io::{BufRead, BufReader, BufWriter, Write}, time};
+
+use base64::{Engine, prelude::BASE64_STANDARD};
 
 mod mhex;
 fn main() {
   let now = time::Instant::now();
-  mhex::input_from_file_real("./input_hex.txt".to_string(),"./output_base64_real.txt".to_string());
+  real_hex_string_file_to_base64_file("./input_hex.txt".to_string(),"./output_base64_real.txt".to_string());
   let dur = now.elapsed();
   println!("Time taken hex to base64 real {:?}",dur);
 
   let now = time::Instant::now();
-  mhex::input_from_file_mine("./input_hex.txt".to_string(),"./output_base64.txt".to_string());
+  real_hex_string_file_to_bin_file("./input_hex.txt".to_string(),"./output_bin_real.txt".to_string());
+  let dur = now.elapsed();
+  println!("Time taken hex to bin real {:?}",dur);
+
+  let now = time::Instant::now();
+  mine_hex_string_file_to_base64_file("./input_hex.txt".to_string(),"./output_base64.txt".to_string());
   let dur = now.elapsed();
   println!("Time taken hex to base64 mine {:?}",dur);
+
+  let now = time::Instant::now();
+  mine_hex_string_file_to_bin_file("./input_hex.txt".to_string(),"./output_bin.txt".to_string());
+  let dur = now.elapsed();
+  println!("Time taken hex to bin mine {:?}",dur);
+}
+
+pub fn real_hex_string_file_to_bin_file(path: String, out: String) {
+  let mut buf = String::new();
+  let mut input = BufReader::new(File::open(path).unwrap());
+  let mut output = BufWriter::new(File::create(out).unwrap());
+  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
+    buf.pop(); // removes new line
+    let hex_bin = hex::decode(&buf).unwrap();
+    for i in hex_bin {
+      output.write_all(format!("{:08b} ",i).as_bytes()).expect("Error while writing to output file");
+    }
+    output.write_all("\n".as_bytes()).expect("Error while writing to output file");
+  }
+}
+
+pub fn mine_hex_string_file_to_bin_file(path: String, out: String) {
+  let mut buf = String::new();
+  let mut input = BufReader::new(File::open(path).unwrap());
+  let mut output = BufWriter::new(File::create(out).unwrap());
+  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
+    buf.pop(); // removes new line
+    let hex_bin = mhex::hex_bytes_to_bytes(buf.as_bytes()).unwrap();
+    for i in hex_bin {
+      output.write_all(format!("{:08b} ",i).as_bytes()).expect("Error while writing to output file");
+    }
+    output.write_all("\n".as_bytes()).expect("Error while writing to output file");
+  }
+}
+
+
+pub fn real_hex_string_file_to_base64_file(path: String, out: String) {
+  let mut buf = String::new();
+  let mut input = BufReader::new(File::open(path).unwrap());
+  let mut output = BufWriter::new(File::create(out).unwrap());
+  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
+    buf.pop(); // removes new line
+    let hex_bin = hex::decode(&buf).unwrap();
+    let base64_string = BASE64_STANDARD.encode(hex_bin);
+    output.write_all(&base64_string.as_bytes()).expect("Error while writing to output file");
+  }
+}
+
+pub fn mine_hex_string_file_to_base64_file(path: String, out: String) {
+  let mut buf = String::new();
+  let mut input = BufReader::new(File::open(path).unwrap());
+  let mut output = BufWriter::new(File::create(out).unwrap());
+  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
+    buf.pop(); // removes new line
+    let base64_string = mhex::hex_to_base64(&buf).unwrap();
+    output.write_all(&base64_string.as_bytes()).expect("Error while writing to output file");
+  }
 }
--- set-01/01-hex-to-base64/hex-to-64/src/mhex.rs
+++ set-01/01-hex-to-base64/hex-to-64/src/mhex.rs
@@ -1,10 +1,7 @@
 use std::{
-  char, fmt::Display, fs::File, io::{BufRead, BufReader, BufWriter, Write}
+  char, fmt::Display
 };
-
-use base64::{Engine, alphabet::STANDARD, prelude::BASE64_STANDARD};
-use byteorder::{ByteOrder, LittleEndian};
-use indicatif::ProgressBar;
+use byteorder::{BigEndian, ByteOrder};
 use itertools::Itertools;
 
 #[derive(Debug, Clone)]
@@ -14,7 +11,7 @@ enum BadHexErrorKind {
 }
 
 #[derive(Debug, Clone)]
-struct BadHexError {
+pub struct BadHexError {
   error_kind: BadHexErrorKind,
   character: char,
   position: usize,
@@ -39,7 +36,7 @@ fn get_hex_val(c1: u8, c2: u8, idx: usize) -> Result<u8, BadHexError> {
   let v1 = match c1 {
     b'A'..=b'F' => (c1 + 9) << 4,
     b'a'..=b'f' => (c1 + 9) << 4,
-    b'0'..=b'9' => c1,
+    b'0'..=b'9' => (c1 & 0b00001111) << 4,
     _ => {
       return Err(BadHexError {
         error_kind: BadHexErrorKind::BadChar,
@@ -51,7 +48,7 @@ fn get_hex_val(c1: u8, c2: u8, idx: usize) -> Result<u8, BadHexError> {
   let res = match c2 {
     b'A'..=b'F' => v1 | (c2 + 9) & 0x0f,
     b'a'..=b'f' => v1 | (c2 + 9) & 0x0f,
-    b'0'..=b'9' => v1 | c2 & 0x0f,
+    b'0'..=b'9' => v1 | (c2 & 0b00001111),
     _ => {
       return Err(BadHexError {
         error_kind: BadHexErrorKind::BadChar,
@@ -63,13 +60,28 @@ fn get_hex_val(c1: u8, c2: u8, idx: usize) -> Result<u8, BadHexError> {
   return Ok(res);
 }
 
-fn hex_to_bytes(hex_string: &str) -> Result<Vec<u8>, BadHexError> {
-  if hex_string.len() % 2 == 1 {
+// pub fn hex_to_bytes(hex_string: &str) -> Result<Vec<u8>, BadHexError> {
+//   if hex_string.len() % 2 == 1 {
+//     // we will assume we can only convert from complete 8 byte chunks
+//     return Err(BadHexError { error_kind: BadHexErrorKind::BadSize, position: 0, character: '\0' });
+//   }
+//   let res = hex_string.as_bytes()
+//     .chunks(2)
+//     .enumerate()
+//     .map(|(i, cs)| match get_hex_val(cs[0], cs[1], i) {
+//       Ok(c) => c,
+//       Err(e) => panic!("{}", e),
+//     })
+//     .collect_vec();
+//   Ok(res)
+// }
+
+pub fn hex_bytes_to_bytes(hex_bytes: &[u8]) -> Result<Vec<u8>, BadHexError> {
+  if hex_bytes.len() % 2 == 1 {
     // we will assume we can only convert from complete 8 byte chunks
     return Err(BadHexError { error_kind: BadHexErrorKind::BadSize, position: 0, character: '\0' });
   }
-  let string_bytes: &[u8] = hex_string.as_bytes();
-  let res = string_bytes
+  let res = hex_bytes
     .chunks(2)
     .enumerate()
     .map(|(i, cs)| match get_hex_val(cs[0], cs[1], i) {
@@ -80,97 +92,40 @@ fn hex_to_bytes(hex_string: &str) -> Result<Vec<u8>, BadHexError> {
   Ok(res)
 }
 
-// fn bytes_to_base64(bytes: Vec<u8>) -> String {
-//   let mut symbols:Vec<char> = Vec::new();
-//   let mut current_byte = 0;
-//   if bytes.len() == 0 {
-//     return "not valid".to_owned();
-//   } else if bytes.len() > 4 {
-//     // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 ** 1111| 1111 ** 11|11 1111  % 4 = 2
-//     // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 ** 1111| 1111 % 4 = 1
-//     // 1111 11|11 ** 1111| 1111 ** 11|11 1111| ** 1111 11|11 % 4 = 0
-//     // 1111 11|11 ** 1111| 1111 ** 11|11 1111  % 4 = 3
-//     // 1111 11|11 ** 1111| 1111 % 4 = 2
-//     // 1111 11|11 % 4 = 1
-//     for i in (0..bytes.len()-2).step_by(3) {
-//       let idx1:usize = ((bytes[i] & 0b1111_1100) >> 2) as usize;
-//       let idx2:usize = ((bytes[i] & 0b0000_0011) << 4) as usize + ((bytes[i+1] & 0b1111_0000) >> 4) as usize;
-//       let idx3:usize = ((bytes[i+1] & 0b0000_1111) << 2) as usize + ((bytes[i+2] & 0b1100_0000) >> 6) as usize;
-//       let idx4:usize = ((bytes[i+2] & 0b0011_1111)) as usize;
-
-//       symbols.push(BASE64_SYMBOLS.chars().nth(idx1).unwrap());
-//       symbols.push(BASE64_SYMBOLS.chars().nth(idx2).unwrap());
-//       symbols.push(BASE64_SYMBOLS.chars().nth(idx3).unwrap());
-//       symbols.push(BASE64_SYMBOLS.chars().nth(idx4).unwrap());
-//       current_byte += 3;
-//     }
-
-//   }
-//   if bytes.len() % 3 == 1 {
-//     // padding twice
-//     symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b1111_1100) >> 2) as usize).unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_0011) << 4) as usize).unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
-//   } else if bytes.len() % 3 == 2 {
-//     // padding once
-//     symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b1111_1100) >> 2) as usize).unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_0011) << 4) as usize + ((bytes[current_byte+1] & 0b1111_0000) >> 4) as usize).unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().nth(((bytes[current_byte] & 0b0000_1111) << 2) as usize).unwrap());
-//     symbols.push(BASE64_SYMBOLS.chars().last().unwrap());
-//   }
-//   return symbols.iter().collect();
-// }
-
-pub fn input_from_file_real(path: String, out: String) {
-  let mut buf = String::new();
-  let mut input = BufReader::new(File::open(path).unwrap());
-  let mut output = BufWriter::new(File::create(out).unwrap());
-  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
-    buf.pop(); // removes new line
-    let base64_string = BASE64_STANDARD.encode(hex::decode(&buf).unwrap());
-    output.write_all(&base64_string.as_bytes()).expect("Error while writing to output file");
+fn hex_bytes_to_base64(hex_bytes: &[u8]) -> Vec<u8> {
+  let bin_codon = hex_bytes_to_bytes(hex_bytes).unwrap();
+  let mut res:Vec<u8> = vec![0,0,0,0];
+  if hex_bytes.len() == 6 {
+    let bin_uint = BigEndian::read_u24(bin_codon.as_ref());
+    res[0] = BASE64_SYMBOLS[((bin_uint & 0x00fc0000) >> 18) as usize];
+    res[1] = BASE64_SYMBOLS[((bin_uint & 0x0003f000) >> 12) as usize];
+    res[2] = BASE64_SYMBOLS[((bin_uint & 0x00000fc0) >> 6) as usize];
+    res[3] = BASE64_SYMBOLS[(bin_uint & 0x0000003f) as usize];
   }
-}
-
-pub fn input_from_file_mine(path: String, out: String) {
-  let mut buf = String::new();
-  let mut input = BufReader::new(File::open(path).unwrap());
-  let mut output = BufWriter::new(File::create(out).unwrap());
-  while input.read_line(&mut buf).expect("Error occured while reading line") != 0 {
-    buf.pop(); // removes new line
-    let base64_string = hex_to_base64(&buf).unwrap();
-    output.write_all(&base64_string.as_bytes()).expect("Error while writing to output file");
+  else if hex_bytes.len() == 4 {
+    let bin_uint = BigEndian::read_u16(bin_codon.as_ref());
+    res[0] = BASE64_SYMBOLS[((bin_uint & 0xfc00) >> 10) as usize];
+    res[1] = BASE64_SYMBOLS[((bin_uint & 0x03f0) >> 4) as usize];
+    res[2] = BASE64_SYMBOLS[((bin_uint & 0x000f) << 2) as usize];
+    res[3] = b'=';
+  } else{
+    let bin_uint = bin_codon[0];
+    res[0] = BASE64_SYMBOLS[((bin_uint & 0xfc) >> 2) as usize];
+    res[1] = BASE64_SYMBOLS[((bin_uint & 0x03) << 4) as usize];
+    res[2] = b'=';
+    res[3] = b'=';
   }
+  res
 }
 
 const BASE64_SYMBOLS: &[u8] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".as_bytes();
 
-fn hex_to_base64(hex_string: &str) -> Result<String, BadHexError> {
+pub fn hex_to_base64(hex_string: &str) -> Result<String, BadHexError> {
   if hex_string.len() % 2 == 1 {
     // we will assume we can only convert from complete 8 bit chunks (a byte)
     return Err(BadHexError { error_kind: BadHexErrorKind::BadSize, position: 0, character: '\0' });
   }
-  let mut base64_res: Vec<u8> = Vec::new() ;
-  let mod_6 = hex_string.len() % 6;
-  //wip 2 or 4 extra hex chars
-  let iterations = hex_string.len() / 6; // 2 and 4 hex characters are the only cases we need to watch out for
-  for i in 0..iterations {
-    let codon = hex_string.get(i..=i+2).unwrap();
-    dbg!("{}",codon);
-    dbg!("{}",i..=i+2);
-    dbg!("{}",iterations);
-    let bin_codon = hex_to_bytes(codon).unwrap();
-    let bin_uint = LittleEndian::read_u48(bin_codon.as_ref());
-    let mut codon_res:Vec<u8> = vec![0,0,0,0];
-    codon_res[0] = BASE64_SYMBOLS[(bin_uint & 0x00fc0000 >> 18) as usize];
-    codon_res[1] = BASE64_SYMBOLS[(bin_uint & 0x0003f000 >> 12) as usize];
-    codon_res[2] = BASE64_SYMBOLS[(bin_uint & 0x00000fc0 >> 6) as usize];
-    codon_res[3] = BASE64_SYMBOLS[(bin_uint & 0x0000003f) as usize];
-    base64_res.append(&mut codon_res);
-  }
-  let mut padding_str = vec![b'=';padding];
-  base64_res.append(&mut padding_str);
+  let res = hex_string.as_bytes().chunks(6).flat_map(hex_bytes_to_base64).collect();
 
-  return Ok(String::from_utf8(base64_res).expect("base64 result is not utf8 valid"));
+  return Ok(String::from_utf8(res).expect("base64 result is not utf8 valid"));
 }
