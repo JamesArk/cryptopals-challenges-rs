39c55420a0c5dfefcc6772fe84f3245135125fa7
JamesArk <thejamesark@gmail.com>
2026-01-19 19:46:43 +0000

    challenge 6 complete

--- src/challenges.rs
+++ src/challenges.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::u32;
 
 use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
@@ -11,52 +13,61 @@ use crate::xor;
 
 use crate::htb64;
 
-pub fn character_frequency_score(sample: String) -> f64 {
-  let freq_table: BTreeMap<u8, f64> = BTreeMap::from([
-    (b'E', 12.7),
-    (b'T', 9.1),
-    (b'A', 8.2),
-    (b'O', 7.5),
-    (b'I', 7.0),
-    (b'N', 6.7),
-    (b'S', 6.3),
-    (b'H', 6.1),
-    (b'R', 6.0),
-    (b'D', 4.3),
-    (b'L', 4.0),
-    (b'C', 2.8),
-    (b'U', 2.8),
-    (b'M', 2.4),
-    (b'W', 2.4),
-    (b'F', 2.2),
-    (b'G', 2.0),
-    (b'Y', 2.0),
-    (b'P', 1.9),
-    (b'B', 1.5),
-    (b'V', 0.98),
-    (b'K', 0.77),
-    (b'J', 0.16),
-    (b'X', 0.15),
-    (b'Q', 0.12),
-    (b'Z', 0.074),
+pub fn character_frequency_score(sample: String) -> u64 {
+  let freq_table: BTreeMap<u8, u64> = BTreeMap::from([
+    (b'E', 1270),
+    (b'T', 910),
+    (b'A', 820),
+    (b'O', 750),
+    (b'I', 700),
+    (b'N', 670),
+    (b'S', 630),
+    (b'H', 610),
+    (b'R', 600),
+    (b'D', 430),
+    (b'L', 400),
+    (b'C', 280),
+    (b'U', 280),
+    (b'M', 240),
+    (b'W', 240),
+    (b'F', 220),
+    (b'G', 200),
+    (b'Y', 200),
+    (b'P', 190),
+    (b'B', 150),
+    (b'V', 98),
+    (b'K', 77),
+    (b'J', 16),
+    (b'X', 15),
+    (b'Q', 12),
+    (b'Z', 7),
   ]);
+  let mut sample_freq_table:HashMap<u8, u64> = HashMap::new();
+  let size = sample.len();
   sample
     .to_ascii_uppercase()
     .as_bytes()
     .iter()
-    .map(|c| freq_table.get(c).unwrap_or(&0.0).clone())
-    .fold(0.0, |acc, e| acc + e)
+    .filter(|c| c.is_ascii_alphabetic())
+    .for_each(|c| {
+      sample_freq_table.entry(c.to_ascii_uppercase()).and_modify(|v| *v+=1).or_insert(1);});
+
+  freq_table.iter().map(|(k,score)| {
+    let count = sample_freq_table.get(k).unwrap_or(&0);
+    score*count/size as u64
+  }).sum::<u64>()
+
 }
 
 #[allow(dead_code)]
 pub fn challenge_3() {
   let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".to_owned();
-  let chars = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n".to_owned();
-  let valid = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n";
+  let chars = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~".to_owned();
+  let valid = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~";
   let cipher = htb64::hex_bytes_to_bytes(input.as_bytes()).unwrap();
-  let mut table: BTreeMap<u8, f64> = BTreeMap::new();
+  let mut table: BTreeMap<u8, u64> = BTreeMap::new();
   for c in chars.as_bytes() {
-    let attempt = String::from_utf8(xor::xor_single_byte(&cipher, c.clone()));
+    let attempt = String::from_utf8(xor::xor_single_byte(&cipher, *c));
     if let Ok(s) = attempt {
       if s.chars().any(|ch| !valid.contains(ch)) {
         continue;
@@ -65,11 +76,11 @@ pub fn challenge_3() {
     }
   }
   let mut res = Vec::from_iter(table);
-  res.sort_by(|&(_, v1), &(_, v2)| v1.total_cmp(&v2).reverse());
+  res.sort_by(|&(_, v1), &(_, v2)| v1.cmp(&v2).reverse());
 
   for (k, v) in res {
-    println!("Character: '{}' | Score:{:.5}", String::from_utf8(vec![k.clone()]).unwrap(), v);
-    println!("Attempt: {:?}", String::from_utf8(xor::xor_single_byte(&cipher, k.clone())).unwrap())
+    println!("Character: '{}' | Score:{:.5}", String::from_utf8(vec![k]).unwrap(), v);
+    println!("Attempt: {:?}", String::from_utf8(xor::xor_single_byte(&cipher, k)).unwrap())
   }
 }
 
@@ -77,9 +88,9 @@ pub fn challenge_3() {
 pub fn challenge_4() {
   let input_file = "./res/challenge_4_extended.txt";
   // let input_file = "./res/challenge_4.txt";
-  let chars = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n".to_owned();
-  let valid = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n";
-  let mut candidates_table: BTreeMap<(usize, String), Vec<(u8, f64, String)>> = BTreeMap::new();
+  let chars = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n\r".to_owned();
+  let valid = "abcdefghijklmnopqrstuvewxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n\r";
+  let mut candidates_table: BTreeMap<(usize, String), Vec<(u8, u64, String)>> = BTreeMap::new();
   let reader =
     BufReader::new(File::open(input_file).expect("Failed to open challenge 4 set 01 input file."))
       .lines()
@@ -89,23 +100,23 @@ pub fn challenge_4() {
     let line = l.expect("Failed to read line from challenge 4 set 01 input file.");
     let cipher_text =
       htb64::hex_bytes_to_bytes(line.as_bytes()).expect("Invalid Hexadecimal string");
-    let mut valid_attempts: Vec<(u8, f64, String)> = Vec::new();
+    let mut valid_attempts: Vec<(u8, u64, String)> = Vec::new();
     for c in chars.as_bytes() {
-      let attempt = String::from_utf8(xor::xor_single_byte(&cipher_text, c.clone()));
+      let attempt = String::from_utf8(xor::xor_single_byte(&cipher_text, *c));
       if let Ok(s) = attempt {
         if s.chars().any(|v| !valid.contains(v)) {
           continue;
         }
-        valid_attempts.push((c.clone(), character_frequency_score(s.clone()), s));
+        valid_attempts.push((*c, character_frequency_score(s.clone()), s));
       }
     }
     if !valid_attempts.is_empty() {
-      valid_attempts.sort_by(|&(_, s1, _), &(_, s2, _)| s1.total_cmp(&s2).reverse());
+      valid_attempts.sort_by(|&(_, s1, _), &(_, s2, _)| s1.cmp(&s2).reverse());
       candidates_table.insert((idx, line), valid_attempts);
     }
   }
   for ((idx, k), v) in candidates_table {
-    if v[0].1 < 90.0 {
+    if v[0].1 < 90 {
       continue;
     }
     println!(
@@ -117,7 +128,7 @@ pub fn challenge_4() {
     for (k, s, p) in top_3_candidates {
       println!(
         "Key: '{}' | Score: {:.5} | Plaintext: {:?}",
-        String::from_utf8(vec![k.clone()]).unwrap(),
+        String::from_utf8(vec![*k]).unwrap(),
         s,
         p
       );
@@ -129,11 +140,13 @@ pub fn challenge_4() {
 #[allow(dead_code)]
 pub fn challenge_6() {
   let input_file = "./res/challenge_6.txt";
+  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n\r".to_owned();
+  let valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@$%^&*()-_+=[{}]'\"\t<>,./?\\|:;`~\n\r".to_owned();
 
   let lines =
     BufReader::new(File::open(input_file).expect("Failed to open input file for challenge 6"))
       .lines();
-  let content: Vec<u8> = lines
+  let base64_ciphertext: Vec<u8> = lines
     .into_iter()
     .map(|v| {
       v.expect("Failed to read line from challenge 6 input file").bytes().collect::<Vec<u8>>()
@@ -141,20 +154,15 @@ pub fn challenge_6() {
     .flatten()
     .collect();
 
-  let content_bytes = BASE64_STANDARD
-    .decode(content)
+  let ciphertext_bytes = BASE64_STANDARD
+    .decode(base64_ciphertext)
     .expect("Failed to decode Base64 string from input file for challenge 6");
   let key_sizes = 2..=40;
 
   let mut scores: BTreeMap<u32, Vec<usize>> = BTreeMap::new();
   for ks in key_sizes {
-
-    let sample: Vec<&[u8]> = content_bytes
-      .chunks(ks)
-      .enumerate()
-      .take_while(|v| v.0 < 30)
-      .map(|(_, v)| v).collect()
-    ;
+    let sample: Vec<&[u8]> =
+      ciphertext_bytes.chunks(ks).enumerate().take_while(|v| v.0 < 30).map(|(_, v)| v).collect();
     let s = sample
       .chunks(2)
       .map(|v| {
@@ -162,14 +170,50 @@ pub fn challenge_6() {
           .expect("Failed to calculate hamming distance between two vecs of bytes in challenge 6")
       })
       .reduce(|v1, v2| v1 + v2)
-      .unwrap_or_default()*1000
-      / (15*ks as u32);
+      .unwrap_or_default()
+      / (15 * ks as u32);
     scores.entry(s).or_default().push(ks);
   }
-  println!("Scores:");
-  for v in scores {
-    println!("{:?}", v);
+  let top:Vec<usize> = scores.iter().take(5).map(|(_,v)| v).flatten().copied().collect();
+  // top 5 scores and their key sizes
+  let mut solutions_table: BTreeMap<usize, (String,String)> = BTreeMap::new();
+  for size in top {
+    let mut cols: Vec<Vec<u8>> = vec![vec![];size];
+    for idx in 0..ciphertext_bytes.len() {
+      cols[idx%size].push(ciphertext_bytes[idx]);
+    }
+    let mut key:Vec<u8> = vec![];
+    for column in cols {
+      let mut char_scores:Vec<(u64,u8)> = Vec::new();
+      for c in chars.as_bytes() {
+        let attempt = String::from_utf8(xor::xor_single_byte(&column,*c));
+        if let Ok(s) = attempt {
+          if s.chars().any(|v| !valid_chars.contains(v)){
+            continue;
+          }
+          char_scores.push((character_frequency_score(s),*c));
+
+        }
+      }
+      if char_scores.is_empty() {
+        continue;
+      }
+      char_scores.sort_by(|(s1,_),(s2,_)| s1.cmp(s2).reverse());
+      let (_,best) = char_scores[0];
+      key.append(&mut vec![best]);
+    }
+    if !key.is_empty() {
+      let plaintext = String::from_utf8(xor::xor_repeating_key(&key, &ciphertext_bytes)).unwrap();
+      solutions_table.insert(size, (String::from_utf8(key).unwrap(),plaintext));
+    }
   }
+  let mut best_solution:(u64,usize,(String,String)) = (0,0,("".to_owned(),"".to_owned()));
+  for (key_size,(key,plaintext)) in solutions_table {
+    let score = character_frequency_score(plaintext.clone());
 
-
+    if best_solution.0 < score {
+      best_solution = (score,key_size,(key,plaintext));
+    }
+  }
+  println!("Key size:{:?}\nKey: {:?}\nPlaintext:\n{}\n---------------------------",best_solution.1,best_solution.2.0,best_solution.2.1);
 }
